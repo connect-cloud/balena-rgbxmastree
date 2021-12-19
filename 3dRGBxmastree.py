f4c9b4925cda5257953f3a6b6d3dda0f30e1350b
connect-cloud <bart@connect-cloud.com>
2021-12-19 17:33:52 +0000

    add Philips Hue connection

--- 3dRGBxmastree.py
+++ 3dRGBxmastree.py
@@ -5,13 +5,35 @@ from colorzero import Color
 import random
 import os
 import signal
+import requests
+import urllib3
+
+urllib3.disable_warnings() # Disable SSL warnings becaus the Hue bridge uses self signed certs
+
 
 
 BRIGHTNESS = (int(os.environ["BRIGHTNESS"])/100) if "BRIGHTNESS" in os.environ else 0.08
-DELAY = float(os.environ["DELAY"]) if "DELAY" in os.environ else 0.5
+DELAY = float(os.environ["DELAY"]) if "DELAY" in os.environ else 1
 STARTTIME = os.environ["STARTTIME"] if "STARTTIME" in os.environ else "0000"
 STOPTIME = os.environ["STOPTIME"] if "STOPTIME" in os.environ else "2359"
 
+if "USE_HUE_LIGHT_STATUS" in os.environ:
+    if os.environ["USE_HUE_LIGHT_STATUS"] == 'True':
+        use_hue_light_status = True
+        hue_ip = os.environ["HUE_IP"] if "HUE_IP" in os.environ else "0.0.0.0"
+        hue_username = os.environ["HUE_USERNAME"] if "HUE_USERNAME" in os.environ else ""
+        hue_light_name = os.environ["HUE_LIGHT_NAME"] if "HUE_LIGHT_NAME" in os.environ else ""
+        if os.environ["HUE_USE_SSL"] == 'True':
+            hue_base_url = f'https://{hue_ip}/api/{hue_username}'
+        else:
+            hue_base_url = f'http://{hue_ip}/api/{hue_username}'
+    else:
+        use_hue_light_status = False
+else:
+    use_hue_light_status = False
+
+
+# Initialize the Tree
 tree = RGBXmasTree(brightness=BRIGHTNESS)
 
 
@@ -19,6 +41,31 @@ def random_color():
     colors = [Color('red'), Color('green'), Color('blue'), Color('yellow'), Color('purple')]
     color = random.choice(colors)
     return color
+
+
+def get_hue_lights(hue_base_url):
+    hue_url = f'{hue_base_url}/lights'
+    response = requests.get(hue_url, verify=False)
+    return response.json()
+
+
+def get_hue_light_by_name(name, hue_base_url):
+    lights = get_hue_lights(hue_base_url)
+    for light_id in lights:
+        light_id = str(light_id) #  convert to string because in the dict the light id is a string
+        if lights[light_id]['name'] == name:
+            return lights[light_id]
+        else:
+            return False
+
+
+def get_hue_light_status_by_name(name, hue_base_url):
+    light = get_hue_light_by_name(name, hue_base_url)
+    if light:
+        if light['state']['on'] and light['state']['reachable']:
+            return True
+    else:
+        return False
 
 
 class GracefulKiller:
@@ -40,21 +87,34 @@ class GracefulKiller:
     tree.close()
     quit()
 
+def color_tree(initialize_tree):
+    if initialize_tree:
+        for pixel in tree:
+            pixel.color = random_color()
+        initialize_tree = False
+    pixel = random.choice(tree)
+    pixel.color = random_color()
+    return initialize_tree
+
 
 if __name__ == "__main__":
-    initial_color = True
+    initialize_tree = True
     killer = GracefulKiller()
     while not killer.kill_now:
         now = datetime.now().strftime("%H%M")
-        if now >= STARTTIME and now < STOPTIME:
-            if initial_color:
-                for pixel in tree:
-                    pixel.color = random_color()
-                initial_color = False
-            pixel = random.choice(tree)
-            pixel.color = random_color()
+        if use_hue_light_status:
+            if get_hue_light_status_by_name(hue_light_name, hue_base_url):
+                print('use hue')
+                initialize_tree = color_tree(initialize_tree)
+                sleep(DELAY)
+            else:
+                tree.off()
+                initialize_tree = True
+                sleep(5)
+        elif now >= STARTTIME and now < STOPTIME:
+            initialize_tree = color_tree(initialize_tree)
             sleep(DELAY)
         else:
             tree.off()
-            initial_color = True
+            initialize_tree = True
             sleep(60)
